c4cbf8558410fd0dac781c207919211d8c4ba0ab
Walt <walt.c0des20@gmail.com>
2023-11-01 10:25:32 -0600

    created basic voting classifier

--- preprocessing_ml.py
+++ preprocessing_ml.py
@@ -57,6 +57,10 @@ def do_ml(ticker):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
     # clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+    
 
     clf.fit(X_train, y_train)
     confidence = clf.score(X_test, y_test)
